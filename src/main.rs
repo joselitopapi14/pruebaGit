7c283b9b0f725fcb6c68f54a898b26d49f67c20a
joselito <enriquejo2002@gmail.com>
2023-12-25 20:45:46 -0500

    djfdjfdbjfdf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+fn primera_vez(){
+    let mut a = String::new();
+    let mut b = String::new();
+
+    println!("Ingrese el primer número");
+    io::stdin()
+        .read_line(&mut a)
+        .expect("Locochon");
+    let a: u32 = a.trim().parse().expect("ingrese un numero correcto");
+
+    println!("Ingrese el segundo número");
+    io::stdin()
+        .read_line(&mut b)
+        .expect("Locochon");
+    let b: u32 = b.trim().parse().expect("ingrese un numero correcto");
+
+    println!("{}",a*b);
+}
+
 fn main() {
-    println!("{}", 2+3);
+    primera_vez();
 }
